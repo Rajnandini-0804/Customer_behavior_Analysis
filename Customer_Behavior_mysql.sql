dfe16232264f3e9661364069af07ec553a56000c
Rajnandini Pravin Bhosale <rajnandinibhosale1234@gmail.com>
2026-01-17 18:46:45 +0530

    Add files via upload

---
+++ Customer_Behavior_mysql.sql
@@ -0,0 +1,102 @@
+SHOW DATABASES;
+CREATE DATABASE customer_behavior;
+USE customer_behavior;
+SELECT COUNT(*) FROM customer;
+
+#Q1. Total revenue by male vs female customers
+SELECT gender, 
+       SUM(purchase_amount) AS revenue
+FROM customer
+GROUP BY gender;
+
+#Q2. Customers who used discount and spent more than average
+SELECT customer_id, purchase_amount
+FROM customer
+WHERE discount_applied = 'Yes'
+  AND purchase_amount > (
+      SELECT AVG(purchase_amount) 
+      FROM customer
+  );
+
+#Q3. Top 5 products with highest average review rating
+SELECT item_purchased,
+       ROUND(AVG(review_rating), 2) AS Average_Product_Rating
+FROM customer
+GROUP BY item_purchased
+ORDER BY AVG(review_rating) DESC
+LIMIT 5;
+
+
+#Q4. Average purchase amount: Standard vs Express shipping
+SELECT shipping_type,
+       ROUND(AVG(purchase_amount), 2) AS avg_purchase
+FROM customer
+WHERE shipping_type IN ('Standard', 'Express')
+GROUP BY shipping_type;
+
+#Q5. Subscriber vs Non-subscriber spend comparison
+SELECT subscription_status,
+       COUNT(customer_id) AS total_customers,
+       ROUND(AVG(purchase_amount), 2) AS avg_spend,
+       ROUND(SUM(purchase_amount), 2) AS total_revenue
+FROM customer
+GROUP BY subscription_status
+ORDER BY total_revenue DESC, avg_spend DESC;
+
+#Q6. Top 5 products with highest discount usage %
+SELECT item_purchased,
+       ROUND(
+           100 * SUM(CASE WHEN discount_applied = 'Yes' THEN 1 ELSE 0 END) 
+           / COUNT(*), 
+       2) AS discount_rate
+FROM customer
+GROUP BY item_purchased
+ORDER BY discount_rate DESC
+LIMIT 5;
+
+#Q7. Customer segmentation: New, Returning, Loyal
+WITH customer_type AS (
+    SELECT customer_id,
+           previous_purchases,
+           CASE
+               WHEN previous_purchases = 1 THEN 'New'
+               WHEN previous_purchases BETWEEN 2 AND 10 THEN 'Returning'
+               ELSE 'Loyal'
+           END AS customer_segment
+    FROM customer
+)
+SELECT customer_segment,
+       COUNT(*) AS Number_of_Customers
+FROM customer_type
+GROUP BY customer_segment;
+
+
+#Q8. Top 3 most purchased products in each category
+WITH item_counts AS (
+    SELECT category,
+           item_purchased,
+           COUNT(*) AS total_orders,
+           ROW_NUMBER() OVER (
+               PARTITION BY category 
+               ORDER BY COUNT(*) DESC
+           ) AS item_rank
+    FROM customer
+    GROUP BY category, item_purchased
+)
+SELECT item_rank, category, item_purchased, total_orders
+FROM item_counts
+WHERE item_rank <= 3;
+
+#Q9. Repeat buyers (>5 purchases) vs subscription
+SELECT subscription_status,
+       COUNT(customer_id) AS repeat_buyers
+FROM customer
+WHERE previous_purchases > 5
+GROUP BY subscription_status;
+
+#Q10. Revenue contribution by age group
+SELECT age_group,
+       SUM(purchase_amount) AS total_revenue
+FROM customer
+GROUP BY age_group
+ORDER BY total_revenue DESC;
